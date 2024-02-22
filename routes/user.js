5f7023a6f1ffa0ff3a9d3e4d5c1653d13baf6e63
AdilMulimani <adilmulimani@gmail.com>
2024-02-22 13:49:16 +0530

    updating register

--- routes/user.js
+++ routes/user.js
@@ -11,78 +11,72 @@ const jwt = require("jsonwebtoken");
 //getting the user model here
 const user_model = require("../models/User.js");
 
+
+
 //posting a request at end point /register
 //authentication and authorization
-router.post("/register",async(req, res)=>{
+router.post('/register', async (req, res, next) => {
     //to store user details we need to get it from client,
     //so we need to pass the parameters to server
     //we can take parameters using req.body or req.query
-    console.log(req.body);
 
-    //when the client registers its data is stored in this object literal
-    const {username,email,password} = req.body;
+    console.log(req.body);
+    const { username, email, password } = req.body;
 
     //checking of user is already registered through email
     //we get exception in async
-    try {//till the user data is fetched we wait
-        let user_exists = await user_model.findOne({email: email});
+    try {
+//till the user data is fetched we wait
+        let user_exist = await user_model.findOne({ email: email });
         //if users email already registered
-        if (user_exists) {
+        if(user_exist) {
             //we send a response that registration was unsuccessful and user already exists
-           return res.json({
+            return res.status(400).json({
                 success: false,
-                message: "User Already Exists"
+                msg: 'User already exists'
             });
-        } else if (!user_exists) {
-            //creating a new instance of user to register user.
-            let user = new user_model();
-            user.username = username;
-            user.email = email;
-            //encrypting password
-            const salt = await bcryptjs.genSalt(10);
-            user.password = await bcryptjs.hash(password, salt);
-            //creating an avatar logo
-            let size = 200;
-            user.avatar = "https://gravatar.com/avatar/?s=" + size + "&d=robohash";
-
-            //saving the user to database
-            await user.save();
-
-            //creating a payload
-            // The payload is used to convey information between parties in a compact and secure manner.
-            //JWTs are commonly used for authentication and authorization in web applications and APIs
-            //The payload typically contains information about the user (such as user ID or roles)
-            //or additional contextual information about the token itself (such as expiration time or issuer).
-
-            const payLoad = {
-                user: {
-                    id: user.id
-                }
-            }
-
-            jwt.sign(payLoad, process.env.JWTUSERSECRETTOKEN, {
-                expiresIn: 360000
-            }, (err, token) => {
-                if (err) throw err;
-                else res.status(200).json({
-                    success: true,
-                    token: token
-                });
-            });
-
-            //     //returning a response when the user is registered
-            //     res.json({
-            //         success:true,
-            //         msg:"User Registered Successfully",
-            //         user:user
-            //     });
-
         }
-    }
-    //if any error occurs we log it
-    catch (err)
-    {
-        console.log(err)
+        //creating a new instance of user to register user.
+        let user = new user_model();
+
+        user.username = username;
+        user.email = email;
+
+        const salt = await bcryptjs.genSalt(10);
+        user.password = await bcryptjs.hash(password, salt);
+
+        let size = 200;
+        user.avatar = "https://gravatar.com/avatar/?s="+size+'&d=robohash';
+
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+
+        jwt.sign(payload, process.env.jwtUserSecret, {
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+
+            res.status(200).json({
+                success: true,
+                token: token
+            });
+        });
+
+
+
+    } catch(err) {
+        console.log(err);
+        res.status(402).json({
+            success: false,
+            message: 'Something error occurred'
+        })
     }
 });
 
